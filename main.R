10085a4ba63260ccdfea5088961a0d12590abb95
Teaching <joey.orofino@gmail.com>
2023-09-27 11:39:40 -0400

    initial commit

---
+++ main.R
@@ -0,0 +1,141 @@
+#!/usr/bin/Rscript
+## Author: Taylor Falk
+## tfalk@bu.edu
+## BU BF591
+## Assignment Week 2
+
+#### Bioconductor ####
+# it is standard among R packages to define libraries and packages at the 
+# beginning of a script. Also note that a package should NOT be installed every 
+# time a script runs.
+# The bioconductor repository has installation instructions for biomaRt: 
+# https://bioconductor.org/install/
+
+if (!require("BiocManager", quietly = TRUE)){
+  install.packages("BiocManager")
+}
+if (!require("biomaRt", quietly = TRUE)){
+  BiocManager::install("biomaRt")
+}
+library(biomaRt)
+library(tidyverse)
+
+#### Loading and processing data ####
+#' Load Expression Data
+#'
+#' @param filepath A text string of the full filepath to the file to load.
+#'
+#' @return A tibble containing the data loaded from the CSV in `filepath`. 
+#' 
+#' @details Note that not all CSVs are created equal, and there are often cases where 
+#' the data will not load in correctly on the first try. You may want to write this functon to 
+#' adjust the CSV file being loaded into this assignment so that it can be formed into a 
+#' tibble correctly.
+#'
+#' @examples 
+#' `data <- load_expression('data/example_intensity_data_subset.csv')`
+load_expression <- function(filepath) {
+  return(NULL)
+}
+
+#' Filter 15% of the gene expression values.
+#'
+#' @param tibble A tibble of expression values, rows by probe and columns by sample.
+#'
+#' @return A tibble of affymetrix probe names from the input expression data tibble. 
+#' These names match the rows with 15% or more of the expression values about log2(15).
+#' 
+#' @details This is similar to the filters being implemented in BF528's project 1. 
+#' We do not necessarily want to capture all parts of the assay in our analysis, so 
+#' filters like this serve to reduce the noise and amount of data to examine.
+#'
+#' @examples `samples <- filter_15(data_tib)`
+#' 
+filter_15 <- function(tibble){
+  return(NULL)
+}
+
+#### Gene name conversion ####
+
+#' Convert affymetrix array names into hgnc_symbol IDs using biomaRt. Inputs and 
+#' outputs will likely not be the same size.
+#'
+#' @param affy_tib A single column tibble of strings containing array names.
+#'
+#' @return A 2 column tibble that contains affy IDs in the first column,
+#' and their corresponding HGNC gene ID in the second column. Note that not all affy IDs 
+#' will necessarily correspond with a gene ID, and one gene may have multiple affy IDs.
+#' 
+#' @details Connecting to ensembl via biomaRt can be...hit or miss...so you may 
+#' want to check if data was correctly returned (or if it was just empty). The 
+#' `getBM()` function may not accept a tibble, so you might need to convert your 
+#' input into a flat vector.
+#'
+#' @examples 
+#' `> affy_to_hgnc(tibble(c('202860_at', '1553551_s_at')))`
+#' `affy_hg_u133_plus_2 hgnc_symbol`
+#' `1        1553551_s_at      MT-ND1`
+#' `2        1553551_s_at       MT-TI`
+#' `3        1553551_s_at       MT-TM`
+#' `4        1553551_s_at      MT-ND2`
+#' `5           202860_at     DENND4B`
+affy_to_hgnc <- function(affy_vector) {
+  return(NULL)
+}
+
+#### ggplot ####
+
+#' Reduce a tibble of expression data to only the rows in good_genes or bad_genes.
+#'
+#' @param expr_tibble A tibble holding the expression data, each row corresponding to
+#' one affymetrix probe ID and each column to a sample.
+#' @param names_ids A two column tibble that associates affy IDs with HGNC gene IDs. 
+#' Generated `with affy_to_hgnc()`.
+#' @param good_genes A list of gene names stored as a vector of strings.
+#' @param bad_genes A list of gene names stored as a vector of strings.
+#'
+#' @return A tibble with two additional columns added:
+#' 1. HGNC gene IDs 
+#' 2. Does the gene is this row fall into "good" or "bad" genes?
+#' This tibble should be reduced to only rows present in good or bad genes. All
+#' other rows can be discarded.
+#' 
+#' @details In order to plot only our genes of interest, we need to rearrange our 
+#' data to include only the elements we want to see. We also want to add to columns, 
+#' one that associates the probeids with the HGNC gene name, and one that says if 
+#' that gene is in the good or bad sets of genes.
+#'
+#' @examples 
+#' `plot_tibble <- reduce_data(expr_tibble = expr, names_ids = sample_names,`
+#' `                           goodGenes, badGenes)`
+#' `> head(plot_tibble)`
+#' `A tibble: 6 × 38`
+#' `  probe       hgnc    gene_set    GSM972389 ...`
+#' `  <chr>       <chr>   <chr>       <dbl>     ...`
+#' `1 202860_at   DENND4B good        7.16      ...`
+#' `2 204340_at   TMEM187 good        6.40      ...`
+reduce_data <- function(expr_tibble, names_ids, good_genes, bad_genes){
+  return(NULL)
+}
+
+#' Convert the tibble from wide to long format.
+#'
+#' @param tibble A tibble of expression data in wide format with information about
+#' good and bad genes, gene names, sample names, and expression values.
+#'
+#' @return A tibble in long format containing the same information.
+#'
+#' @details This function's primary objective is to reformat the tibble from a 
+#' wide format, where there are separate columns for sample name and expression value, 
+#' to a long format, where each column represents a sample.
+#'
+#' @examples 
+#' # Assuming 'wide_data' is your wide-format tibble
+#' long_data <- convert_to_long(wide_data)
+#' head(long_data)
+convert_to_long <- function(tibble) {
+  # Convert long format to wide format
+  return(NULL)
+}
+
+
