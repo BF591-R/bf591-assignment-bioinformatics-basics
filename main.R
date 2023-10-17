496afaeb862bbda49442c7378bdc46eaa9b4e731
joeyorofino <joeyorofino@bf591-admin-tools.deploy-assignment>
2023-10-17 14:43:37 +0000

    See https://github.com/BF591-R/bf591-assignment-bioinformatics-basics-source/commit/6a21a673c503240755ec8e188017e41f23a1c8a6 from refs/heads/main

--- main.R
+++ main.R
@@ -2,7 +2,7 @@
 ## Author: Taylor Falk
 ## tfalk@bu.edu
 ## BU BF591
-## Assignment Week 2
+## Assignment Bioinformatics Basics
 
 #### Bioconductor ####
 # it is standard among R packages to define libraries and packages at the 
@@ -17,8 +17,8 @@ if (!require("BiocManager", quietly = TRUE)){
 if (!require("biomaRt", quietly = TRUE)){
   BiocManager::install("biomaRt")
 }
-library(biomaRt)
-library(tidyverse)
+suppressPackageStartupMessages(library(biomaRt))
+suppressPackageStartupMessages(library(tidyverse))
 
 #### Loading and processing data ####
 #' Load Expression Data
@@ -27,10 +27,15 @@ library(tidyverse)
 #'
 #' @return A tibble containing the data loaded from the CSV in `filepath`. 
 #' 
+#' @details Note that not all CSVs are created equal, and there are often cases where 
+#' the data will not load in correctly on the first try. You may want to write this functon to 
+#' adjust the CSV file being loaded into this assignment so that it can be formed into a 
+#' tibble correctly.
+#'
 #' @examples 
-#' `data <- load_expression('data/example_intensity_data_subset.csv')`
+#' `data <- load_expression('/project/bf528/project_1/data/example_intensity_data.csv')`
 load_expression <- function(filepath) {
-  return(NULL)
+    return(NULL)
 }
 
 #' Filter 15% of the gene expression values.
@@ -40,14 +45,13 @@ load_expression <- function(filepath) {
 #' @return A tibble of affymetrix probe names from the input expression data tibble. 
 #' These names match the rows with 15% or more of the expression values about log2(15).
 #' 
-#' @details This is similar to the filters being implemented in BF528's project 1. 
-#' We do not necessarily want to capture all parts of the assay in our analysis, so 
-#' filters like this serve to reduce the noise and amount of data to examine.
 #'
 #' @examples `samples <- filter_15(data_tib)`
-#' 
+#' `> str(samples)`
+#' `tibble [40,158 × 1] (S3: tbl_df/tbl/data.frame)`
+#' `$ probe: chr [1:40158] "1007_s_at" "1053_at" "117_at" "121_at" ...`
 filter_15 <- function(tibble){
-  return(NULL)
+    return(NULL)
 }
 
 #### Gene name conversion ####
@@ -66,11 +70,17 @@ filter_15 <- function(tibble){
 #' `getBM()` function may not accept a tibble, so you might need to convert your 
 #' input into a flat vector.
 #'
+#' @examples 
+#' `> affy_to_hgnc(tibble(c('202860_at', '1553551_s_at')))`
+#' `affy_hg_u133_plus_2 hgnc_symbol`
+#' `1        1553551_s_at      MT-ND1`
+#' `2        1553551_s_at       MT-TI`
+#' `3        1553551_s_at       MT-TM`
+#' `4        1553551_s_at      MT-ND2`
+#' `5           202860_at     DENND4B`
 affy_to_hgnc <- function(affy_vector) {
-  return(NULL)
+    return(NULL)
 }
-
-#### ggplot ####
 
 #' Reduce a tibble of expression data to only the rows in good_genes or bad_genes.
 #'
@@ -92,25 +102,30 @@ affy_to_hgnc <- function(affy_vector) {
 #' one that associates the probeids with the HGNC gene name, and one that says if 
 #' that gene is in the good or bad sets of genes.
 #'
+#' @examples 
+#' `plot_tibble <- reduce_data(expr_tibble = expr, names_ids = sample_names,`
+#' `                           goodGenes, badGenes)`
+#' `> head(plot_tibble)`
+#' `A tibble: 6 × 38`
+#' `  probeids    hgnc    gene_set    GSM972389 ...`
+#' `  <chr>       <chr>   <chr>       <dbl>     ...`
+#' `1 202860_at   DENND4B good        7.16      ...`
+#' `2 204340_at   TMEM187 good        6.40      ...`
 reduce_data <- function(expr_tibble, names_ids, good_genes, bad_genes){
-  return(NULL)
+    return(NULL)
 }
 
-#' Convert the tibble from wide to long format.
+#' Convert a wide format tibble to long for easy plotting
 #'
-#' @param tibble A tibble of expression data in wide format with information about
-#' good and bad genes, gene names, sample names, and expression values.
+#' @param tibble A tibble of data in wide format. Specifically, it should be 
+#' operating on the reduced tibble created by the previous function
 #'
-#' @return A tibble in long format containing the same information.
+#' @return A tibble properly converted from wide to long format, the old sample 
+#' columns should now be contained within a single column called "sample"
+#' @export
 #'
-#' @details This function's primary objective is to reformat the tibble from a 
-#' wide format, where there are separate columns for sample name and expression value, 
-#' to a long format, where sample names are stored in a column named 'sample' 
-#' and their values stored in another column named 'value'.
-#'
+#' @examples
 convert_to_long <- function(tibble) {
-  # Convert wide format to long format
-  return(NULL)
+    return(NULL)
 }
-
 
--- test_main.R
+++ test_main.R
@@ -3,13 +3,18 @@
 source("main.R")
 library(testthat)
 
-test_that("loading csv works using load_expression()", {
-  result_tib <- load_expression("data/example_intensity_data_subset.csv")
-  expect_equal(dim(result_tib), c(1000, 36))
-  expect_true(is_tibble(result_tib))
+describe("load_expression()", {
+  result_tib <- load_expression('data/example_intensity_data_subset.csv')
+  
+  it("returns a tibble, not a dataframe", {
+    expect_true(is_tibble(result_tib))
+  })
+  it("returns a tibble with the right dimensions", {
+    expect_equal(dim(result_tib), c(1000, 36))
+  })
 })
 
-test_that("the correct rows are filtered out in filter_15()", {
+describe("filter_15()", {
   test_tib <- tibble(probe=c('1_s_at', '2_s_at', '3_s_at', '4_s_at'),
                      GSM1=c(1.0, 3.95, 4.05, 0.5),
                      GSM2=rep(1.6, 4),
@@ -18,11 +23,13 @@ test_that("the correct rows are filtered out in filter_15()", {
                      GSM5=rep(3.0, 4),
                      GSM6=rep(1.0, 4),
                      GSM7=rep(0.5, 4))
-  expect_equal(c(filter_15(test_tib)$probe), c("2_s_at", "3_s_at"))
+  
+  it("correctly filters the rows meeting the specified criteria", {
+    expect_equal(c(filter_15(test_tib)$probe), c("2_s_at", "3_s_at"))
+  })
 })
 
-test_that("affy ids can be converted to HGNC names properly using affy_to_hgnc()", {
-  # biomaRt super buggy so we can try to account for not connecting well
+describe("affy_to_hgnc()", {
   success <- FALSE
   attempt <- 0
   while (!success ) {
@@ -37,10 +44,13 @@ test_that("affy ids can be converted to HGNC names properly using affy_to_hgnc()
     })
     if (attempt == 3) stop("Process halted. Function faced non-connection errors. Try again later.")
   }
-  expect_equal(response$hgnc_symbol, c("MT-ND1", "MT-TI", "MT-TM", "MT-ND2"))
+  
+  it("returns the right identifiers for the chosen probe, 1553551_s_at", {
+    expect_equal(response$hgnc_symbol, c("MT-ND1", "MT-TI", "MT-TM", "MT-ND2"))
+  })
 })
 
-test_that("reduce_data() is correctly changing the size and shape of the tibble", {
+describe("reduce_data()", {
   t_tibble <- tibble(probe = c("1_s_at", "2_s_at", "3_s_at"),
                      GSM1 = c(9.5, 7.6, 5.5),
                      GSM2 = c(9.7, 7.2, 2.9),
@@ -49,37 +59,43 @@ test_that("reduce_data() is correctly changing the size and shape of the tibble"
                   hgnc_symbol = c("A-REAL-GENE", "SONIC", "UTWPU"))
   good <- c("A-REAL-GENE")
   bad <- c("SONIC")
+  
   reduce_test <- reduce_data(t_tibble, names, good, bad)
-  result <- tibble(probe = c("1_s_at", "2_s_at"),
+  expected_result <- tibble(probe = c("1_s_at", "2_s_at"),
                    hgnc_symbol = c("A-REAL-GENE", "SONIC"),
                    gene_set = c("good", "bad"),
                    GSM1 = c(9.5, 7.6),
                    GSM2 = c(9.7, 7.2),
                    GSM3 = c(6.9, 4.3))
-  expect_equal(reduce_test, result)
+  
+  it("returns a reduced tibble with the specified columns", {
+    expect_equal(reduce_test, expected_result)
+  })
 })
 
-test_that("convert_to_long() correctly converts from wide to long format", {
-  
+describe("convert_to_long()", {
   # wide format tibble
   wide_tib <- tibble(probe = c("202274_at", "202541_at", "202542_s_at", "203919_at"),
-                         hgnc = c("ACTG2", "AIMP1", "AIMP1", "TCEA2"),
-                         gene_set = rep("good", 4),
-                         GSM1 = c(8.05, 8.40, 9.55, 4.44),
-                         GSM2 = c(7.74, 7.11, 8.48, 5.39))
+                     hgnc = c("ACTG2", "AIMP1", "AIMP1", "TCEA2"),
+                     gene_set = rep("good", 4),
+                     GSM1 = c(8.05, 8.40, 9.55, 4.44),
+                     GSM2 = c(7.74, 7.11, 8.48, 5.39))
   
   # expected long format tibble
   expected_tib <- tibble(probe = rep(c("202274_at", "202541_at", "202542_s_at", "203919_at"), each = 2),
-                     hgnc = rep(c("ACTG2", "AIMP1", "AIMP1", "TCEA2"), each = 2),
-                     gene_set = rep(rep("good", 4), each = 2),
-                     sample = rep(c("GSM1", "GSM2"), 4),
-                     value = c(8.05, 7.74, 8.40, 7.11, 9.55, 8.48, 4.44, 5.39))
+                         hgnc = rep(c("ACTG2", "AIMP1", "AIMP1", "TCEA2"), each = 2),
+                         gene_set = rep(rep("good", 4), each = 2),
+                         sample = rep(c("GSM1", "GSM2"), 4),
+                         value = c(8.05, 7.74, 8.40, 7.11, 9.55, 8.48, 4.44, 5.39))
   
-  # Convert to long format
   long_format <- convert_to_long(wide_tib)
   
-  # Test if the converted long format matches the expected tibble
-  expect_equal(long_format, expected_tib)
+  #this tests for shape
+  it("correctly converts a wide format tibble to long format", {
+    expect_equal(long_format, expected_tib)
+  })
+  #this tests for column named properly
+  it("contains a column named sample", {
+    expect_true("sample" %in% colnames(long_format))
+  })
 })
-
-
